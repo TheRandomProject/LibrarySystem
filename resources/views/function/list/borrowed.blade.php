8551daca7f1e814a95c0cda4ee7adf15ea783e81
John Paul <28johnpaulsim28@gmail.com>
2021-03-04 20:22:23 +0800

    fix code layout

--- resources/views/function/list/borrowed.blade.php
+++ resources/views/function/list/borrowed.blade.php
@@ -24,18 +24,18 @@
                 @if(count($borroweds) > 0)
                     @foreach($borroweds as $borrowed)
                         @if ($borrowed->created_at == $borrowed->updated_at)
-                            <tr>
+                            <tr class="bg-danger text-white">
                                 <td>{{$loop->iteration}}</td>
                                 <td>{{$borrowed->student->firstname}}, {{$borrowed->student->lastname}}</td>
                                 <td>{{$borrowed->book->title}}</td>
                                 <td>{{$borrowed->student->email}}</td>
                                 <td>{{$borrowed->student->contact}}</td>
-                                <td>{{$borrowed->request}}</td>
                                 <td>{{$borrowed->created_at}}</td>
                                 <td> </td>
+                                <td>{{$borrowed->request}}</td>
                                 <td>
                                     {!! Form::open(['route' => ['admin.borroweds.update', $borrowed->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                                        <input type="hidden" name="request" value="approve">
+                                        <input type="hidden" name="request" value="Taken">
                                         {{Form::hidden('_method','PUT')}}
                                         <button type="submit" class="btn btn-primary btn-sm" title="approve"><i class="fas fa-check"></i></button>
                                     {!!Form::close()!!}
@@ -49,17 +49,10 @@
                                 <td>{{$borrowed->book->title}}</td>
                                 <td>{{$borrowed->student->email}}</td>
                                 <td>{{$borrowed->student->contact}}</td>
-                                <td>{{$borrowed->request}}</td>
                                 <td>{{$borrowed->created_at}}</td>
+                                <td>{{$borrowed->updated_at}}</td>
+                                <td>{{$borrowed->request}}</td>
                                 <td>
-                                    {{$borrowed->updated_at}}
-                                </td>
-                                <td>
-                                    {!! Form::open(['route' => ['admin.borroweds.update', $borrowed->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                                        <input type="hidden" name="request" value="approve">
-                                        {{Form::hidden('_method','PUT')}}
-                                        <button type="submit" class="btn btn-primary btn-sm" title="approve"><i class="fas fa-check"></i></button>
-                                    {!!Form::close()!!}
                                     <button type="submit" class="btn btn-danger btn-sm" title="reject"><i class="fas fa-times"></i></button>
                                 </td>
                             </tr>
